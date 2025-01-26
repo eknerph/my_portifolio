cfbe7e2040265425453c996205300832138d4453
eknerph <eknerph@gmail.com>
2025-01-26 20:04:01 -0300

    feat iteirando components information

--- src/app/components/information/informatin.tsx
+++ src/app/components/information/informatin.tsx
@@ -1,11 +1,21 @@
-interface InfoProps {
+"/information.scss"
 
 }
-
-
 export function Info(props : InfoProps) {
   return {
-    <></>
+    <div className="infos">
+    <h3>Languages</h3>
+    <div className="languages-info">
+      <span> 🇺🇸 EN - Fluent</span>
+      <span> 🇧🇷 PT-BR - Native Speaker</span>
+    </div>
+    <h3>Education</h3>
+    <div className="educational-info">
+    <span>🎓</span>
+    <span>Technologist in Systems Analysis and Development - Unifran Graduação EAD</span>
+
+    </div>
+  </div>
   }
   
 }
