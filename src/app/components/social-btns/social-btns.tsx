6cb8822eac5fb994478bdcc38a02c12742acdeb9
eknerph <eknerph@gmail.com>
2025-02-02 21:53:43 -0300

    feat: AAdicionado social className em social-btns

--- src/app/components/social-btns/social-btns.tsx
+++ src/app/components/social-btns/social-btns.tsx
@@ -3,6 +3,10 @@ interface SocialBtnsProps {
 }
 export default function SocialBtns(props : SocialBtnsProps){
     return(
-        <.................></>
+        <div className="social">
+            
+            
+            
+        </div>
     )
 }
