b4d2b7d2ec4c499a5e78d6d09b77941425fe8bb0
eknerph <eknerph@gmail.com>
2025-02-04 22:35:26 -0300

    feat: Adicionado link GithubIcon

--- src/app/components/social-btns/social-btns.tsx
+++ src/app/components/social-btns/social-btns.tsx
@@ -1,4 +1,6 @@
+import { GithubIcon } from "../icons/github-icon";
 import { InstaIcon } from "../icons/insta-icon";
+import { LinkedinIcon } from "../icons/linkedin-icon";
 
 interface SocialBtnsProps {
 
@@ -10,10 +12,14 @@ export default function SocialBtns(props : SocialBtnsProps){
                 <InstaIcon/>
             </a>
             <a href="">
-                LinkedinIcon
+                <LinkedinIcon/>
             </a>
-            <a href=""></a>
-            <a href=""></a>
+            <a href="">
+                <GithubIcon/>
+            </a>
+            <a href="">
+                
+            </a>
             
             
             
