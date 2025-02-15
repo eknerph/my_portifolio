aa1b8e2c749bda1ce5bdc5c915a7fb578aa318a2
eknerph <eknerph@gmail.com>
2025-02-14 23:38:25 -0300

    feat: Adicionado react.png

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -8,7 +8,13 @@ export function Experience(){
           <p>06 Meses de experiência adiquido em treinamento para desenvolvimento pessoal e terceiros.</p>
           <div className="experience-time">
             <div>
-              
+              <Image
+                src="/react.svg"
+                alt="Next.js logo"
+                width={325}
+                height={317}
+                priority 
+              />        
             </div>
           </div>
         </div>
